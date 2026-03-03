03f11be38543d1f00f0ca7f4da3def33d7abbda3
Francisco Afonso <afonso.webdev@gmail.com>
2026-03-03 15:13:38 +0000

    Alterações feitas em projetos e em formulario de contato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 // main.js
 import { initProjectModal } from "./modal.js";
+import { initValidation } from "./validation.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initProjectModal();
+  initValidation();
 });
